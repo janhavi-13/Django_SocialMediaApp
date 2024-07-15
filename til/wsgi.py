525513b09a83aee7cc5ae13dae0a0dc58d3d6607
janhavi-13 <75656691+janhavi-13@users.noreply.github.com>
2024-07-15 19:56:06 +0530

    Update wsgi.py

--- til/wsgi.py
+++ til/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'til.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '
+BreadcrumbsDjango_SocialMediaApp.settings')
 
 application = get_wsgi_application()
